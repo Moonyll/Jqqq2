33c63ce680907f0bbf408ce40f1e16fcea5f69ab
Moonyll <s.pouwel@laposte.net>
2019-01-17 15:47:26 +0100

    Jqqq2

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    var compteur = 0;
+    $('#plus').click(function () {
+        compteur++;
+        $('#text').html('Vous avez cliqué ' + compteur + ' fois.');
+    });
+    $('#moins').click(function () {
+        compteur--;
+        $('#text').html('Vous avez cliqué ' + compteur + ' fois.');
+    });
+});
